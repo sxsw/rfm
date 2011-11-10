e0d05ab4b3f886fd71096f64f03423cb76bed223
wbr <wbr@mac.com>
2011-11-09 17:04:09 -0800

    Modeling branch version to 1.5.0.pre4

--- lib/rfm/version.rb
+++ lib/rfm/version.rb
@@ -1,9 +1,9 @@
 module Rfm
   module Version
     MAJOR = 1
-    MINOR = 4
-    PATCH = 2
-    BUILD = 'modeling3'
+    MINOR = 5
+    PATCH = 0
+    BUILD = 'pre4'
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
   end
