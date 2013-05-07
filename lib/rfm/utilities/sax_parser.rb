196383895f08ffc8f9d175d2beddb3c36ffa1583
wbr <wbr@mac.com>
2013-05-06 18:08:19 -0700

    Modified dev-readme & todos.

--- lib/rfm/utilities/sax_parser.rb
+++ lib/rfm/utilities/sax_parser.rb
@@ -59,7 +59,7 @@ require 'stringio'
 # done: Fill in other configuration options in yml
 # done: Clean_elements doesn't work if elements are non-hash/array objects. Make clean_elements work with object attributes.
 # TODO: Clean_elements may not work for non-hash/array objecs with multiple instance-variables.
-# TODO: Clean_elements may no longer work with a globally defined 'compact'.
+# na  : Clean_elements may no longer work with a globally defined 'compact'.
 # TODO: Do we really need Cursor#top and Cursor#stack ? Can't we get both from stack?
 # TODO: When using a non-hash/array object as the initial object, things get kinda srambled.
 #       See Rfm::Connection, when sending self (the connection object) as the initial_object.
@@ -67,10 +67,10 @@ require 'stringio'
 # na  : Do the same thing with 'ignore' as you did with 'attach', so you can enable using the 'attributes' array of the yml model.
 # done: Double-check that you're pointing to the correct model/submodel, since you changed all helper-methods to look at curent-model by default.
 # done: Make sure all method calls are passing the model given by the calling-method.
-# TODO: Give most of the config options a global possibility.
+# abrt: Give most of the config options a global possibility. (no true global config, but top-level acts as global for all immediate submodels).
 # na  : Block attachment methods from seeing parent if parent isn't the current objects true parent (how?).
 # done: Handle attach: hash better (it's not specifically handled, but declaring it will block a parents influence).
-# TODO: CaseInsensitiveHash/IndifferentAccess is not working for sax parser.
+# done: CaseInsensitiveHash/IndifferentAccess is not working for sax parser.
 # na  : Give the yml (and xml) doc the ability to have a top-level hash like "fmresultset" or "fmresultset_yml" or "fmresultset_xml",
 #       then you have a label to refer to it if you load several config docs at once (like into a Rfm::SaxParser::TEMPLATES constant).
 #       Use an array of accepted model-keys  to filter whether loaded template is a named-model or actual model data.
@@ -104,8 +104,8 @@ require 'stringio'
 # done: Can't supply custom class if it's a String (but an unspecified subclass of plain Object works fine!?).
 # TODO: Attaching non-hash/array object to Array will thro error. Is this actually fixed?
 # done?: Sending elements with subelements to Shared results in no data attached to the shared var.
-# TODO: compact is not working for fmpxmllayout-error
-# TODO: Add ability to put a regex in the as_name parameter, that will operate on the tag/label/name.
+# TODO: compact is not working for fmpxmllayout-error. Consider rewrite of 'compact' method.
+# mabe: Add ability to put a regex in the as_name parameter, that will operate on the tag/label/name.
 # TODO: Optimize:
 #				Use variables, not methods.
 #				Use string interpolation not concatenation.
@@ -117,11 +117,12 @@ require 'stringio'
 # done: Check resultset portal_meta in Splash Asset model for field-definitions coming out correct according to sax template.
 # done: Make sure Rfm::Metadata::Field is being used in portal-definitions.
 # done: Scan thru Rfm classes and use @instance variables instead of their accessors, so sax-parser does less work.
-# TODO: Change 'instance' option to 'private'. Change 'shared' to <whatever>.
+# done: Change 'instance' option to 'private'. Change 'shared' to <whatever>.
 # done: Since unattached elements won't callback, add their callback to an array of procs on the current cursor at the beginning
 #				of the non-attached tag.
-# TODO: Handle check-for-errors in Rfm class loading.
-# abort: Figure out way to get doctype from nokogiri. Tried, may be practically impossible.
+# TODO: Handle check-for-errors in non-resultset loads.
+# abrt: Figure out way to get doctype from nokogiri. Tried, may be practically impossible.
+# TODO: Clean up sax code so that no 'rescue' is needed - if an error happens it should be a legit error outside of SaxParser.
 
 
 
