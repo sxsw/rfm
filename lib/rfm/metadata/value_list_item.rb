60c172cf644e9b4dc03d1ce0dc19f1c98e011d07
wbr <wbr@mac.com>
2013-05-10 15:41:35 -0700

    Fixing fmpxmllayout parsing.

--- lib/rfm/metadata/value_list_item.rb
+++ lib/rfm/metadata/value_list_item.rb
@@ -15,14 +15,15 @@ module Rfm
 	  #
 	  # * *#value_list_name* is the name of the parent value list, if any
 	  class ValueListItem < String
+	  	# TODO: re-instate saving of value_list_name.
 	    attr_reader :value, :display, :value_list_name
 	    
-	    def initialize(value, display, value_list_name)
-	      @value_list_name = value_list_name
-	      @value					 = value.to_s
-	      @display				 = display.to_s
-	      self.replace @value
-	    end
+			#   def initialize(value, display, value_list_name)
+			#     @value_list_name = value_list_name
+			#     @value					 = value.to_s
+			#     @display				 = display.to_s
+			#     self.replace @value
+			#   end
 	  
 	  end # ValueListItem
 	  
