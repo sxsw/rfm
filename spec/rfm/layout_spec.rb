49a7ea9c4a41bbec6bac724c88e4f0d13305aeb7
Jon Roberts <jon@emptyflask.net>
2014-07-31 13:52:22 -0500

    update deprecated specs with transpec

--- spec/rfm/layout_spec.rb
+++ spec/rfm/layout_spec.rb
@@ -6,71 +6,71 @@ describe Rfm::Layout do
   let(:name)     {'test_layout'}
   let(:layout)   {Rfm::Layout.new(name, database)}
 	before(:each) do
-		server.stub!(:state).and_return({})
-		database.stub!(:server).and_return(server)
+		allow(server).to receive(:state).and_return({})
+		allow(database).to receive(:server).and_return(server)
 		#data.stub!(:body).and_return(RESULTSET_XML)
 		#meta.stub!(:body).and_return(LAYOUT_XML)
 	end
 		
 	describe "#initialze" do
 		it "should set configuration" do
-			layout.config[:layout].should == name
-			layout.config[:parent].should == database
-			layout.instance_variable_get(:@loaded).should == false
+			expect(layout.config[:layout]).to eq(name)
+			expect(layout.config[:parent]).to eq(database)
+			expect(layout.instance_variable_get(:@loaded)).to eq(false)
 		end
 	end # initialize
 	
 	describe "#get_records" do
 		before(:each) do
-			Rfm::Connection.stub(:new).and_return(Rfm::Connection.allocate)
-			Rfm::Connection.any_instance.stub(:parse).and_return(Rfm::Resultset.allocate)
-			Rfm::Layout.any_instance.stub(:capture_resultset_meta)
+			allow(Rfm::Connection).to receive(:new).and_return(Rfm::Connection.allocate)
+			allow_any_instance_of(Rfm::Connection).to receive(:parse).and_return(Rfm::Resultset.allocate)
+			allow_any_instance_of(Rfm::Layout).to receive(:capture_resultset_meta)
 		end
 
 		it "calls Connection.new with action, params, and options" do
-			Rfm::Connection.should_receive(:new) do |*args|
-				args[0].should == "-find"
-				args[1].should == {"-db" => nil, "-lay" => name, :prms=>'test'}
-				args[2].should == {:opts=>'test'}
-			end.and_return(Rfm::Connection.allocate)
-			layout.should_receive(:capture_resultset_meta)
+			expect(Rfm::Connection).to receive(:new) { |*args|
+				expect(args[0]).to eq("-find")
+				expect(args[1]).to eq({"-db" => nil, "-lay" => name, :prms=>'test'})
+				expect(args[2]).to eq({:opts=>'test'})
+			}.and_return(Rfm::Connection.allocate)
+			expect(layout).to receive(:capture_resultset_meta)
 
 			layout.send :get_records, "-find", {:prms=>'test'}, {:opts=>'test'}
 		end
 		
 		
 		it "calls connection.parse with parameters" do
-			Rfm::Connection.any_instance.should_receive(:parse) do |*args|
-				args[0].should == :fmresultset
-				args[1].class.should == Rfm::Resultset
+			expect_any_instance_of(Rfm::Connection).to receive(:parse) do |instance, *args|
+				expect(args[0]).to eq(:fmresultset)
+				expect(args[1].class).to eq(Rfm::Resultset)
 			end
 			layout.send(:get_records, '-find', {:prms=>'tst'}, {:opts=>'tst'})
 		end
 		
 		it "returns instance of Resultset" do
-			layout.send(:get_records, '-find', {:prms=>'tst'}, {:opts=>'tst'}).class.should == Rfm::Resultset
+			expect(layout.send(:get_records, '-find', {:prms=>'tst'}, {:opts=>'tst'}).class).to eq(Rfm::Resultset)
 		end
 		
 		it "translates field names, given field-translation map" do
 			layout.config :field_mapping => {'userName' => 'login'}
 			
-			Rfm::Connection.should_receive(:new) do |*args|
-				args[1].has_key?('login').should be_false
-				args[1].has_key?('username').should be_true
-				args[1]['username'].should == 'bill'
-			end.and_return(Rfm::Connection.allocate)
-			layout.field_mapping['userName'].should == 'login'
+			expect(Rfm::Connection).to receive(:new) { |*args|
+				expect(args[1].has_key?('login')).to be_falsey
+				expect(args[1].has_key?('username')).to be_truthy
+				expect(args[1]['username']).to eq('bill')
+			}.and_return(Rfm::Connection.allocate)
+			expect(layout.field_mapping['userName']).to eq('login')
 			
 			layout.send(:get_records, '-find', {'login'=>'bill'})
 		end
 		
 		it "builds repeating-field query params from repeating-field array" do			
-			Rfm::Connection.should_receive(:new) do |*args|
-				args[1].has_key?('repeating').should be_false
-				args[1].has_key?('repeating(1)').should be_true
-				args[1].has_key?('repeating(3)').should be_true
-				args[1]['repeating(3)'].should == 'bill'
-			end.and_return(Rfm::Connection.allocate)
+			expect(Rfm::Connection).to receive(:new) { |*args|
+				expect(args[1].has_key?('repeating')).to be_falsey
+				expect(args[1].has_key?('repeating(1)')).to be_truthy
+				expect(args[1].has_key?('repeating(3)')).to be_truthy
+				expect(args[1]['repeating(3)']).to eq('bill')
+			}.and_return(Rfm::Connection.allocate)
 			
 			layout.send(:get_records, '-edit', {'repeating'=>['mike','amy','bill']})
 		end
@@ -82,7 +82,7 @@ describe Rfm::Layout do
 			# This is now handled in spec_helper
 			#Rfm::Connection.any_instance.stub(:connect).and_return LAYOUT_XML
 			layout.send(:load_layout)
-			layout.field_controls.has_key?('stayid').should be_true
+			expect(layout.field_controls.has_key?('stayid')).to be_truthy
 		end
 		
 		it "loads layout meta with value_lists" do
@@ -90,7 +90,7 @@ describe Rfm::Layout do
 			#Rfm::Connection.any_instance.stub(:connect).and_return LAYOUT_XML
 			layout.send(:load_layout)
 			#puts layout.to_yaml
-			layout.value_lists.has_key?('employee unique id').should be_true
+			expect(layout.value_lists.has_key?('employee unique id')).to be_truthy
 		end
 	end
 	
@@ -98,7 +98,7 @@ describe Rfm::Layout do
 		it "returns resultset containing instance of Rfm::Record" do
 			# This is now handled in spec_helper
 			#Rfm::Connection.any_instance.stub(:connect).and_return(RESULTSET_XML)
-			layout.send(:any)[0].is_a?(Rfm::Record).should be_true
+			expect(layout.send(:any)[0].is_a?(Rfm::Record)).to be_truthy
 		end
 	end
 	
@@ -106,9 +106,9 @@ describe Rfm::Layout do
 	
 		context "when passed FMP internal record id" do
 			it "sends -find action & -recid & record id to #get_records" do
-				layout.should_receive(:get_records) do |action, query, options|
-					action.should == '-find'
-					query['-recid'].should == '54321'
+				expect(layout).to receive(:get_records) do |action, query, options|
+					expect(action).to eq('-find')
+					expect(query['-recid']).to eq('54321')
 				end
 				layout.find(54321)			
 			end
@@ -116,9 +116,9 @@ describe Rfm::Layout do
 		
 		context "when passed plain hash" do
 			it "sends -find action & plain hash to #get_records" do
-				layout.should_receive(:get_records) do |action, query, options|
-					action.should == '-find'
-					query[:memotext].should == 'val1'
+				expect(layout).to receive(:get_records) do |action, query, options|
+					expect(action).to eq('-find')
+					expect(query[:memotext]).to eq('val1')
 				end
 				layout.find(:memotext=>'val1', :memosubject=>'val2')			
 			end		
@@ -126,9 +126,9 @@ describe Rfm::Layout do
 
 		context "when passed plain hash with multiple value options for at least one field" do
 			it "sends -findquery action & compound find criteria to #get_records" do
-				layout.should_receive(:get_records) do |action, query, options|
-					action.should == '-findquery'
-					['(q0,q2);(q1,q2)','(q0,q1);(q0,q2)'].include?(query['-query']).should be_true
+				expect(layout).to receive(:get_records) do |action, query, options|
+					expect(action).to eq('-findquery')
+					expect(['(q0,q2);(q1,q2)','(q0,q1);(q0,q2)'].include?(query['-query'])).to be_truthy
 				end
 				layout.find(:memotext=>['one','two'], :memosubject=>'three')
 			end
@@ -136,9 +136,9 @@ describe Rfm::Layout do
 		
 		context "when passed array of hashes" do
 			it "sends -findquery action & compound find criteria to #get_records" do
-				layout.should_receive(:get_records) do |action, query, options|
-					action.should == '-findquery'
-					['(q0,q2);(q1,q2);!(q3)','(q0,q1);(q0,q2);!(q3)'].include?(query['-query']).should be_true
+				expect(layout).to receive(:get_records) do |action, query, options|
+					expect(action).to eq('-findquery')
+					expect(['(q0,q2);(q1,q2);!(q3)','(q0,q1);(q0,q2);!(q3)'].include?(query['-query'])).to be_truthy
 				end
 				layout.find([{:memotext=>['one','two'], :memosubject=>'three'}, {:omit=>true, :memotext=>'test'}])
 			end
@@ -149,16 +149,16 @@ describe Rfm::Layout do
 		before(:each){layout.modelize}
 		
 		it "creates model subclassed from Rfm::Base" do
-			layout.models[0].should == Rfm::TestLayout
-			layout.models[0].ancestors.include?(Rfm::Base).should be_true
+			expect(layout.models[0]).to eq(Rfm::TestLayout)
+			expect(layout.models[0].ancestors.include?(Rfm::Base)).to be_truthy
 		end
 		
 		it "stores model in layout@model as constant based on layout name" do
-			layout.models[0].should == Rfm::TestLayout
+			expect(layout.models[0]).to eq(Rfm::TestLayout)
 		end
 		
 		it "sets model@layout with layout object" do
-			layout.models[0].layout.should == layout
+			expect(layout.models[0].layout).to eq(layout)
 		end
 	end
 
--- spec/rfm/record_spec.rb
+++ spec/rfm/record_spec.rb
@@ -15,7 +15,7 @@ describe Rfm::Record do
 				rs = Rfm::Resultset.new @Layout, @Layout
 				#puts rs.layout
 				#puts rs.layout.model
-				Rfm::Record.new(rs).class.should == Memo
+				expect(Rfm::Record.new(rs).class).to eq(Memo)
 			end
 		end
 		
@@ -37,14 +37,14 @@ describe Rfm::Record do
       record['tester'] = ''
       record.instance_variable_set(:@loaded, true)
       
-      record['tester'].should eql('')
+      expect(record['tester']).to eql('')
     end
     
     it "returns nil if hash value is nil " do
       record['tester'] = nil
       record.instance_variable_set(:@loaded, true)
       
-      record['tester'].should eql(nil)
+      expect(record['tester']).to eql(nil)
     end
     
     it "raises an Rfm::ParameterError if a key is used that does not exist" do
@@ -52,21 +52,21 @@ describe Rfm::Record do
       record.instance_variable_set(:@layout, layout) # will allow this test to pass
 
       ex = rescue_from { record['tester2'] }
-      ex.class.should eql(Rfm::ParameterError)
-      ex.message.should eql('tester2 does not exists as a field in the current Filemaker layout.')
+      expect(ex.class).to eql(Rfm::ParameterError)
+      expect(ex.message).to eql('tester2 does not exists as a field in the current Filemaker layout.')
     end
     
     it "returns value whether key is string or symbol" do
       record.instance_variable_set(:@loaded, true)
       
-      record.has_key?(:tester).should be_false
-      record[:tester].should eql('red')
+      expect(record.has_key?(:tester)).to be_falsey
+      expect(record[:tester]).to eql('red')
     end
     
     it "returns value regardless of key case" do
     	record.instance_variable_set(:@loaded, true)
     	
-    	record['TESTER'].should == 'red'
+    	expect(record['TESTER']).to eq('red')
     end
   end #[]
 
@@ -79,51 +79,51 @@ describe Rfm::Record do
     end
     
     it "creates a new hash key => value upon instantiation of record" do
-      record.has_key?('tester').should be_true
-      record['tester'].should eql('red')
+      expect(record.has_key?('tester')).to be_truthy
+      expect(record['tester']).to eql('red')
     end
     
     it "creates a new hash key as downcase" do
     	record['UPCASE'] = 'downcase'
-    	record.key?('upcase').should be_true
+    	expect(record.key?('upcase')).to be_truthy
     end
     
     it "creates a new hash key => value in @mods when modifying an existing record key" do
       record.instance_variable_set(:@loaded, true)
       record['tester'] = 'green'
       
-      record.instance_variable_get(:@mods).has_key?('tester').should be_true
-      record.instance_variable_get(:@mods)['tester'].should eql('green')
+      expect(record.instance_variable_get(:@mods).has_key?('tester')).to be_truthy
+      expect(record.instance_variable_get(:@mods)['tester']).to eql('green')
     end
     
     it "modifies the hash key => value in self whether key is string or symbol" do
       record.instance_variable_set(:@loaded, true)
       record[:tester] = 'green'
       
-      record.has_key?(:tester).should be_false
-      record['tester'].should eql('green')
+      expect(record.has_key?(:tester)).to be_falsey
+      expect(record['tester']).to eql('green')
     end
     
     it "returns '' if hash value is '' " do
       record['tester'] = 'something'
       record.instance_variable_set(:@loaded, true)
       record['tester'] = ''
-      record['tester'].should eql('')
+      expect(record['tester']).to eql('')
     end
     
     it "returns nil if hash value is nil " do
       record['tester'] = 'something'
       record.instance_variable_set(:@loaded, true)
       record['tester'] = nil
-      record['tester'].should eql(nil)
+      expect(record['tester']).to eql(nil)
     end
     
     it "raises an Rfm::ParameterError if a value is set on a key that does not exist" do
       record.instance_variable_set(:@loaded, true)
 
       ex = rescue_from { record['tester2'] = 'error' }
-      ex.class.should eql(Rfm::ParameterError)
-      ex.message.should match(/You attempted to modify a field.*that does not exist in the current Filemaker layout/)
+      expect(ex.class).to eql(Rfm::ParameterError)
+      expect(ex.message).to match(/You attempted to modify a field.*that does not exist in the current Filemaker layout/)
     end
   end #[]=
   
@@ -132,11 +132,11 @@ describe Rfm::Record do
     it "returns true if key is in hash" do
       record['red'] = 'stop'
       
-      record.respond_to?(:red).should be_true
+      expect(record.respond_to?(:red)).to be_truthy
     end
     
     it "returns false if key is not in hash" do
-      record.respond_to?(:red).should be_false
+      expect(record.respond_to?(:red)).to be_falsey
     end
   end
   
@@ -148,13 +148,13 @@ describe Rfm::Record do
     
     describe "getter" do
       it "will match a method to key in the hash if there is one" do
-        record.name.should eql('red')
+        expect(record.name).to eql('red')
       end
 
       it "will raise NoMethodError if no key present that matches value" do
         ex = rescue_from { record.namee }
-        ex.class.should eql(NoMethodError)
-        ex.message.should match(/undefined method `namee'/)
+        expect(ex.class).to eql(NoMethodError)
+        expect(ex.message).to match(/undefined method `namee'/)
       end
     end
     
@@ -163,14 +163,14 @@ describe Rfm::Record do
       	record.instance_variable_set(:@loaded, true)
         record.name = 'blue'
 
-        record.instance_variable_get(:@mods).has_key?('name').should be_true
-        record.instance_variable_get(:@mods)['name'].should eql('blue')
+        expect(record.instance_variable_get(:@mods).has_key?('name')).to be_truthy
+        expect(record.instance_variable_get(:@mods)['name']).to eql('blue')
       end
       
       it "will raise NoMethodError if no key present that matches value" do
         ex = rescue_from { record.namee = 'red' }
-        ex.class.should eql(NoMethodError)
-        ex.message.should match(/undefined method `namee='/)
+        expect(ex.class).to eql(NoMethodError)
+        expect(ex.message).to match(/undefined method `namee='/)
       end
     end
 
@@ -183,38 +183,38 @@ describe Rfm::Record do
 	  	record.instance_variable_set(:@loaded, true)
 	  	record.instance_variable_set(:@layout, layout)
 	  	record.instance_variable_set(:@mods, {})
-	  	layout.stub!(:edit).and_return{[record.dup.merge(record.instance_variable_get(:@mods))]}
+	  	allow(layout).to receive(:edit){[record.dup.merge(record.instance_variable_get(:@mods))]}
     end
     
   	context "when not modified" do
 			let(:original) {record.dup}
 			let(:result) {record.save}
 			
-			it("leaves self untouched"){should == original}
-			it("returns {}"){result.should == {}}			
+			it("leaves self untouched"){is_expected.to eq(original)}
+			it("returns {}"){expect(result).to eq({})}			
   	end
    
   	context "when modified" do
 			before(:each) {record.name = 'green'}
 			
 			it "passes @mods and @mod_id to layout" do
-	    	layout.should_receive(:edit).with(1, {'name'=>'green'})
+	    	expect(layout).to receive(:edit).with(1, {'name'=>'green'})
 	    	record.save
  			end
  			
  			it "merges returned hash from Layout#edit" do
  				record.instance_variable_get(:@mods)['name'] = 'blue'
  				record.save
- 				record['name'].should eql('blue')
+ 				expect(record['name']).to eql('blue')
  			end
  			
  			it "clears @mods" do
  				record.save
- 				record.instance_variable_get(:@mods).should eql({})
+ 				expect(record.instance_variable_get(:@mods)).to eql({})
  			end
  			
  			it "returns {}" do
- 				record.save.should eql({})
+ 				expect(record.save).to eql({})
  			end
   	end
   	
@@ -228,38 +228,38 @@ describe Rfm::Record do
 			record.instance_variable_set(:@mod_id, 5)
 			record.instance_variable_set(:@layout, layout)
 			record.instance_variable_set(:@mods, {})
-			layout.stub!(:edit).and_return{[record.instance_variable_get(:@mods)]}
+			allow(layout).to receive(:edit){[record.instance_variable_get(:@mods)]}
 		}
 		
 		context "when local record not modified" do
 			let(:original) {record.dup}
 			let(:result) {record.save_if_not_modified}
 			
-			it("leaves self untouched"){should == original}
-			it("returns {}"){result.should == {}}			
+			it("leaves self untouched"){is_expected.to eq(original)}
+			it("returns {}"){expect(result).to eq({})}			
 		end
 		
 		context "when local record modified" do
 			before(:each) {record.name = 'green'}
 			
 			it "passes @mods and @mod_id to layout" do
-	    	layout.should_receive(:edit).with(1, {'name'=>'green'}, {:modification_id => 5})
+	    	expect(layout).to receive(:edit).with(1, {'name'=>'green'}, {:modification_id => 5})
 	    	record.save_if_not_modified
  			end
  			
  			it "merges returned hash from Layout#edit" do
  				record.instance_variable_get(:@mods)['name'] = 'blue'
  				record.save_if_not_modified
- 				record['name'].should eql('blue')
+ 				expect(record['name']).to eql('blue')
  			end
  			
  			it "clears @mods" do
  				record.save_if_not_modified
- 				record.instance_variable_get(:@mods).should eql({})
+ 				expect(record.instance_variable_get(:@mods)).to eql({})
  			end
  			
  			it "returns {}" do
- 				record.save_if_not_modified.should eql({})
+ 				expect(record.save_if_not_modified).to eql({})
  			end
 		end
 		
